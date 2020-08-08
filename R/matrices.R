bd56f82675f4ae83ec30f9896c45896b7a140a4d
Steve Martin <stevemartin041@gmail.com>
2020-08-08 14:55:15 -0400

    Make rs_z work with length 0 inputs

--- R/matrices.R
+++ R/matrices.R
@@ -23,7 +23,7 @@ rs_z <- function(t2, t1, f, sparse = FALSE) {
     if (requireNamespace('Matrix', quietly = TRUE)) {
       mm <- Matrix::sparse.model.matrix
     } else {
-      stop('The Matrix library is not installed.')
+      stop("The 'Matrix' library is not installed.")
     }
     # otherwise use the regular dense model matrix
   } else {
@@ -31,10 +31,9 @@ rs_z <- function(t2, t1, f, sparse = FALSE) {
   }
   # turn inputs into factors
   lev <- sort(unique(c(as.character(t2), as.character(t1))))
-  # throw an error if there are fewer than 2 levels
-  if (length(lev) < 2L) stop("There must be at least two levels in 't2' and 't1'")
   t2 <- factor(t2, lev)
   t1 <- factor(t1, lev)
+  nm <- if (!is.null(names(t2))) names(t2) else if (!is.null(names(t1))) names(t1) else seq_along(t2)
   # throw a warning if any t2 < t1
   if (any(as.numeric(t2) <= as.numeric(t1))) {
     warning("All elements of 't2' should be greater than the corresponding elements in 't1'")
@@ -46,9 +45,15 @@ rs_z <- function(t2, t1, f, sparse = FALSE) {
     t1 <- f:t1
   }
   # calculate Z
-  z <- mm(~ t2 - 1) - mm(~ t1 - 1)
-  # set useful column names
-  colnames(z) <- levels(t2)
+  z <- if (nlevels(t2) < 2L) {
+    # return a matrix of 0's if there's only one level
+    (if (sparse) Matrix::Matrix else matrix)(rep(0, length(t2)), ncol = 1)
+  } else {
+    mm(~ t2 - 1) - mm(~ t1 - 1)
+  }
+  # set useful names
+  colnames(z) <- if (nlevels(t2) > 0) levels(t2)
+  rownames(z) <- if (length(nm) > 0) nm
   # remove model.matrix attributes
   attributes(z)[c('assign', 'contrasts')] <- NULL
   z
