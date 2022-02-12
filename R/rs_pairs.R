711779def15806b0a08a9183884dc876be4db4df
Steve Martin <stevemartin041@gmail.com>
2022-02-12 00:20:28 -0500

    rs_pairs() is now much faster

--- R/rs_pairs.R
+++ R/rs_pairs.R
@@ -1,25 +1,20 @@
-# internal function to find previous value for an integer vector
-prev <- function(x) {
-  ord <- order(x, na.last = NA)
-  res <- rep.int(NA_integer_, length(x))
-  res[ord] <- ord[c(1L, seq_len(length(ord) - 1L))]
-  res
-}
-
 rs_pairs <- function(period, product) {
   if (length(period) != length(product)) {
     stop(gettext("'period' and 'product' must be the same length"))
   }
   if (!length(period)) return(integer(0L))
   period <- as.factor(period)
-  attributes(period) <- NULL # faster to use numeric codes
   product <- as.factor(product)
-  res <- split(seq_along(period), product)
-  period <- split(period, product)
-  if (max(vapply(period, anyDuplicated, numeric(1L), incomparables = NA))) {
-    warning(gettext("there are duplicated period-product pairs"))
-  }
-  m <- lapply(period, prev)
-  res <- .mapply(`[`, list(res, m), list())
-  unsplit(res, product)
+  # faster to use numeric codes
+  attributes(period) <- NULL
+  attributes(product) <- NULL
+  ord <- order(product, period, na.last = NA)
+  res <- rep.int(NA_integer_, length(period))
+  # offset the period by product ordering
+  res[ord] <- ord[c(1L, seq_len(length(ord) - 1L))]
+  # the first period for each product points to the last period
+  # for the previous product
+  first <- which(product[res] != product)
+  res[first] <- first
+  res
 }
--- tests/tests_pairs.R
+++ tests/tests_pairs.R
@@ -1,14 +1,67 @@
 library(rsmatrix)
 
 #---- Tests for finding previous values ----
-prev2 <- function(x) {
-  dr <- match(x, sort(unique(x)))
-  match(pmax(dr - 1L, 1L), dr, incomparables = NA)
+# internal function to find previous value for an integer vector
+prev <- function(x) {
+  ord <- order(x, na.last = NA)
+  res <- rep.int(NA_integer_, length(x))
+  res[ord] <- ord[c(1L, seq_len(length(ord) - 1L))]
+  res
 }
 
-set.seed(1432)
-x <- sample(1e3)
-y <- replace(x, sample(1e3, 10), NA)
+# previous method
+rs_pairs2 <- function(period, product) {
+  if (length(period) != length(product)) {
+    stop(gettext("'period' and 'product' must be the same length"))
+  }
+  if (!length(period)) return(integer(0L))
+  period <- as.factor(period)
+  attributes(period) <- NULL # faster to use numeric codes
+  product <- as.factor(product)
+  res <- split(seq_along(period), product)
+  period <- split(period, product)
+  if (max(vapply(period, anyDuplicated, numeric(1L), incomparables = NA))) {
+    warning(gettext("there are duplicated period-product pairs"))
+  }
+  m <- lapply(period, prev)
+  res <- .mapply(`[`, list(res, m), list())
+  unsplit(res, product)
+}
 
-all.equal(rsmatrix:::prev(x), prev2(x))
-all.equal(rsmatrix:::prev(y), prev2(y))
+# An easy-to-verify test
+# y x  x_prev pos
+# b 1  1      1
+# c 10 7      10
+# a 1  1      3
+# a 2  1      3
+# a 3  2      4
+# b 3  2      7
+# b 2  1      1
+# d 1  1      8
+# c 11 10     2
+# c 7  7      10
+# d 1  1      8
+
+x <- c(1, 10, 1:3, 3:2, 1, 11, 7, 1)
+y <- letters[c(2, 3, 1, 1, 1, 2, 2, 4, 3, 3, 4)]
+rs_pairs(x, y)
+
+x[2] <- NA
+rs_pairs(x, y)
+
+y[5] <- NA
+rs_pairs(x, y)
+
+# Should return an integer
+typeof(rs_pairs(x, y))
+
+rs_pairs(numeric(0), character(0))
+
+# Test against an older implementation
+set.seed(4321)
+
+all(replicate(100, {
+  x <- replace(sample(1e3), sample(1e3, 25), NA)
+  y <- replace(sample(letters, 1e3, TRUE), sample(1e3, 100), NA)
+  all.equal(rs_pairs(x, y), rs_pairs2(x, y))
+}))
